92df423d3d7d0aefc074c16bc9c25da6c5767055
skibugvt <skibug@gmail.com>
2014-10-11 22:22:06 -0400

    Plot 3 v2

--- Project 2 (week 3)/Plot3.R
+++ Project 2 (week 3)/Plot3.R
@@ -2,8 +2,11 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+#Subset the data for baltimore
+baltimore_data<-subset(NEI, fips == "24510")
+
 #Aggregate data
-aggdata<- aggregate(Emissions ~ year+type, data=NEI, sum)
+aggdata<- aggregate(Emissions ~ year+type, data=baltimore_data, sum)
 
 #plot the data w/regression line
 myplot<-qplot(year,Emissions,data=aggdata,facets=type~.,geom = c("point", "smooth"), method = "lm",main="Total Emissions from PM2.5 in US by Year")
