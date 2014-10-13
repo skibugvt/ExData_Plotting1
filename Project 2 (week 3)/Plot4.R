72a6e2106d686d2e4813dcb2d63f8d55e12926de
skibugvt <skibug@gmail.com>
2014-10-12 20:57:14 -0400

    Plot 4

---
+++ Project 2 (week 3)/Plot4.R
@@ -0,0 +1,20 @@
+#Read data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Find Coal Related Combustion.
+coal<-grep("Fuel Comb.*Coal",SCC$EI.Sector,ignore.case=TRUE)
+coal_scc<-SCC[coal,"SCC"]
+
+#Subset the data for coal
+coal_data<-subset(NEI, SCC %in% coal_scc)
+
+#Aggregate data
+aggdata<- aggregate(Emissions ~ year, data=coal_data, sum)
+
+#plot the data w/regression line
+myplot<-qplot(year,Emissions,data=aggdata,geom = c("point", "smooth"), method = "lm",main="Total Emissions from PM2.5 from Coal Combustion in US by Year")
+png(filename="Plot4.png")
+print(myplot)
+dev.off()
+
